c9510265c522c84116dd1ee94eedb922e2ef90e9
Dmitry Shabeko <shabeko.d@gmail.com>
2022-05-19 00:29:47 +0300

    first commit

---
+++ jsssssss/js/script.js
@@ -0,0 +1,12 @@
+"use district"
+
+let storeName = "sbk";
+let storeDescription = {
+   budget: 10000,
+   employees: ["Jim", "Mike", "John"],
+   products: {
+      milk: 10,
+      suger: 5
+   },
+   open: true
+};
